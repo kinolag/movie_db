980797d429f11b711a3f48b32aa73d0b25d25a6a
Nicola <ngrassini@yahoo.it>
2024-04-12 16:03:53 +0100

    • create Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <h3>Movie DB App</h3>
+      <p>
+        developed by{" "}
+        <a
+          href="https://github.com/kinolag"
+          target="_blank"
+          rel="noreferrer"
+          title="Developer's GitHub page"
+        >
+          kinolag
+        </a>
+      </p>
+    </header>
+  );
+}
